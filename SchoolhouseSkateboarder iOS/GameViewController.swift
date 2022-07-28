2ae66bcea79a7e17c861a2083dd7f6bacfb455f1
Evgenii <109528582+Praktikant18@users.noreply.github.com>
2022-07-28 13:53:56 +0700

    Add files via upload
    SchoolhouseSkateboarder iOS

---
+++ SchoolhouseSkateboarder iOS/GameViewController.swift
@@ -0,0 +1,46 @@
+//
+//  GameViewController.swift
+//  SchoolhouseSkateboarder iOS
+//
+//  Created by Евгений Цуриков on 11.01.2022.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene()
+        let width = view.bounds.width
+        let height = view.bounds.height
+        scene.size = CGSize(width: width, height: height)
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
